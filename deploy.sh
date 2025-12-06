d408c526f617029abb648d8042851fbe2ebbb512
Nate <secondarynate@icloud.com>
2025-12-06 13:05:49 -0600

    updated scheme + added yt-feeeds

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ panic() {
 upload_pkg() {
     (
         cd "$1" || panic "Could not cd into ${1}"
+        rm -rf "${1}"*
         CHECKSUM=$(makepkg -g) || panic "Could not genererate checksum"
 
         sed -i "s/^pkgver=.*/pkgver=${2}/" PKGBUILD || panic "Coud not set version"
@@ -25,8 +26,8 @@ upload_pkg() {
 }
 
 VERSION=$(curl -s "https://api.github.com/repos/nate-craft/auditorium/releases/latest" | jq -r .tag_name)
-upload_pkg "auditorium" "$VERSION" "auditorium-v${VERSION}-linux-amd64"
-upload_pkg "auditorium-minimal" "$VERSION" "auditorium-minimal-v${VERSION}-linux-amd64"
+upload_pkg "auditorium" "$VERSION" "auditorium-${VERSION}-x86_64-unknown-linux-gnu"
+upload_pkg "auditorium-minimal" "$VERSION" "auditorium-minimal-${VERSION}-x86_64-unknown-linux-gnu"
 
 VERSION=$(curl -s "https://api.github.com/repos/nate-craft/yt-feeds/releases/latest" | jq -r .tag_name)
 upload_pkg "yt-feeds" "$VERSION" "yt-feeds-${VERSION}-x86_64-unknown-linux-gnu"
